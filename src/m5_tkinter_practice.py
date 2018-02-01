07f6be066ac03b60ace2e17feed50393a944fdcb
fleethrd <fleethrd@rose-hulman.edu>
2018-02-01 11:09:33 -0500

    done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,8 +2,8 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Ryan Fleetham.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -12,28 +12,28 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
@@ -42,7 +42,7 @@ def main():
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -64,6 +64,54 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+
+    root = tkinter.Tk()
+
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
+
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
+
+    integer_entry = ttk.Entry(frame1)
+    integer_entry.grid()
+
+    print_entry_button = ttk.Button(frame1, text='Print entry')
+    print_entry_button['command'] = (lambda:
+                                     print_contents(my_entry_box))
+    print_entry_button.grid()
+
+    second_button = ttk.Button(frame1, text='Second button')
+    second_button['command'] = (lambda:
+                                print_second_button(my_entry_box))
+    second_button.grid()
+
+    third_button = ttk.Button(frame1, text='Third button')
+    third_button['command'] = (lambda:
+                               print_third_button(my_entry_box, integer_entry))
+    third_button.grid()
+
+    root.mainloop()
+
+
+def print_contents(entry_box):
+    contents_of_entry_box = entry_box.get()
+    print(contents_of_entry_box)
+
+
+def print_second_button(entry_box):
+    contents_of_entry_box = entry_box.get()
+    if contents_of_entry_box == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
+
+
+def print_third_button(entry_box, integer_box):
+    contents_of_entry_box = entry_box.get()
+    contents_of_integer_box = int(integer_box.get())
+    for k in range(contents_of_integer_box):
+        print(contents_of_entry_box)
 
     # ------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
